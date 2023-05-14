197686970bdef533c3e85fda8165f038781153b7
semaed <figsaned@gmail.com>
2023-05-14 23:41:33 +0000

    Correct betty error

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -4,7 +4,7 @@
  * binary_tree_insert_right - Inserts a node as the right-child of another node
  * @parent: Pointer to the node to insert the right-child in
  * @value: The value to store in the new node
- * 
+ *
  * Return: Pointer to the created node, or NULL on failure or if parent is NULL
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)

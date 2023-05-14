187088da275b73733cf8892b4717393f7e4923e8
nashalysf <nashalysf@gmail.com>
2023-05-14 19:48:38 +0000

    initial commit

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_node - creates a binary tree node
+ * @parent: pointer to parent node
+ * @value: value of new node
+ * Return: pointer to new node or NULL
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+
+}

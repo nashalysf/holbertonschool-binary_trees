a877223b98f5b5e0e0a0b19c10fc83b1f32e6785
semaed <figsaned@gmail.com>
2023-05-15 00:27:01 +0000

    Correction to the code for betty

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -10,13 +10,13 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	/*Check if the node is NULL*/
 	if (node == NULL)
-		return 0;
+		return (0);
 
 	/*Check if the left child of the node is NULL*/
 	if (node->left == NULL && node->right == NULL)
 		/*If the node is a leaf return 1*/
-		return 1;
-	else
-		/*If the node is not a leaf return 0*/
-		return 0;
+		return (1);
+
+	/*If the node is not a leaf return 0*/
+	return (0);
 }
